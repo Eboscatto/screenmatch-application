38274a8727feb8490daf8f7a56407e2717f64da9
Eboscaatto <e_boscatto@hotmail.com>
2024-05-09 14:29:48 -0300

    Criada primeira rota da API

---
+++ src/main/java/br/com/everaldoboscatto/screenmatch/dto/SerieDTO.java
@@ -0,0 +1,15 @@
+package br.com.everaldoboscatto.screenmatch.dto;
+
+import br.com.everaldoboscatto.screenmatch.model.Categoria;
+import jakarta.persistence.*;
+
+public record SerieDTO(Long id,
+                       String titulo,
+                       Integer totalTemporadas,
+                       Double avaliacao,
+                       Categoria genero,
+                       String atores,
+                       String poster,
+                       String sinopse) {
+
+}
